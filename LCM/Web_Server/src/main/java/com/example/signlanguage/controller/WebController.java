88ebe2c7a59400de76421b39907100f889b5b445
lecocococo <chemwang13@naver.com>
2022-09-22 18:17:10 +0900

    Feat: MongoDB Connect

--- LCM/Web_Server/src/main/java/com/example/signlanguage/controller/WebController.java
+++ LCM/Web_Server/src/main/java/com/example/signlanguage/controller/WebController.java
@@ -1,16 +1,24 @@
 package com.example.signlanguage.controller;
 
+import com.example.signlanguage.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
 public class WebController {
+    private final RoomRepository repository;
 
     @GetMapping(value = "/hospital")
-    public String hospital(){
-        System.out.println("IN");
-        return "hospital/hospital";
+    public ModelAndView hospital() {
+        System.out.println("Chat Room Created!!!!");
+        ModelAndView mv = new ModelAndView("hospital/hospital");
+
+        mv.addObject("list", repository.findAll());
+
+        return mv;
+        // return "hospital/hospital";
     }
 }
